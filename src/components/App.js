4278866d74640c0dabec88cb0ee2a28ac61a61d0
Bertie Spell <berniesnell@gmail.com>
2017-03-22 09:30:35 +0000

    project initialised

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = React.createClass({
+    render: function () {
+        return (
+            <div>
+                APP!!
+                </div>
+        );
+    }
+});
+
+export default App;
