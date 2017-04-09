85a493f259b06d846dfd167dda60903747bc8c69
Bertie Spell <berniesnell@gmail.com>
2017-04-09 10:27:29 +0100

    updated config to mLab db

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   DB: {
     test: 'mongodb://localhost/northcoders-news-api-test',
-    dev: 'mongodb://localhost/northcoders-news-api'
+    dev: 'mongodb://Bertiespell:q1w2e3r4t5@ds157390.mlab.com:57390/northcoders_news'
   },
   PORT: {
     test: 3090,
